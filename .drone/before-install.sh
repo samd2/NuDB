49f1dbc92d66e3d4ba7aecf0ae6dd120927feffc
sdarwin <samuel.d.darwin@gmail.com>
2021-02-05 21:00:49 +0000

    test

---
+++ .drone/before-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Copyright 2020 Rene Rivera, Sam Darwin
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+if [ "$DRONE_JOB_UUID" = "b6589fc6ab" ] || [ "$DRONE_JOB_UUID" = "da4b9237ba" ] || [ "$DRONE_JOB_UUID" = "77de68daec" ] || [ "$DRONE_JOB_UUID" = "1b64538924" ] || [ "$DRONE_JOB_UUID" = "ac3478d69a" ] || [ "$DRONE_JOB_UUID" = "c1dfd96eea" ] || [ "$DRONE_JOB_UUID" = "902ba3cda1" ] || [ "$DRONE_JOB_UUID" = "fe5dbbcea5" ] || [ "$DRONE_JOB_UUID" = "0ade7c2cf9" ] || [ "$DRONE_JOB_UUID" = "b1d5781111" ] || [ "$DRONE_JOB_UUID" = "17ba079149" ] || [ "$DRONE_JOB_UUID" = "7b52009b64" ] || [ "$DRONE_JOB_UUID" = "fa35e19212" ] ; then
+    if [ "${MATRIX_EVAL}" != "" ] ; then eval "${MATRIX_EVAL}"; fi
+fi
+if [ "$DRONE_JOB_UUID" = "356a192b79" ] ; then
+    ln -s /usr/bin/nodejs /usr/bin/node
+    echo "deb https://deb.nodesource.com/node_6.x xenial main" > /etc/apt/sources.list.d/nodesource.list ;
+    echo "deb-src https://deb.nodesource.com/node_6.x xenial main" >> /etc/apt/sources.list.d/nodesource.list;
+    curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add - ;
+    sudo apt-get update ;
+    apt-get install -y nodejs;
+    if [ "${MATRIX_EVAL}" != "" ] ; then eval "${MATRIX_EVAL}"; fi
+fi
+if [ "$DRONE_JOB_UUID" = "bd307a3ec3" ] ; then
+    export UBSAN_SYMBOLIZER_PATH="$(which llvm-symbolizer-8)"
+    if [ "${MATRIX_EVAL}" != "" ] ; then eval "${MATRIX_EVAL}"; fi
+fi
+
